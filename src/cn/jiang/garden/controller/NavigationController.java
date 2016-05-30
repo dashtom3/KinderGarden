c3f32a665fea71b8ddbd80f2b3c7a3f573ee0260
dashtom3 <395750080@qq.com>
2016-05-30 13:19:01 +0800

    Merge branch 'master' of /Users/tia/Desktop/KinderGarden with conflicts.

--- src/cn/jiang/garden/controller/NavigationController.java
+++ src/cn/jiang/garden/controller/NavigationController.java
@@ -1,30 +1,16 @@
 package cn.jiang.garden.controller;
 
-import cn.jiang.garden.DAO.TUserDao;
-import cn.jiang.garden.model.TTokenEntity;
-import cn.jiang.garden.model.TUserEntity;
-import cn.jiang.garden.utils.MD5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.sql.Date;
-
+/**
+ * Created by tia on 16/5/28.
+ */
 @Controller
 public class NavigationController {
-    @Autowired
-    TUserDao userDao;
     @RequestMapping(value="/home")
     public String mainPage(){
-        TUserEntity user = new TUserEntity();
-        user.setApplicationId(new Long(1));
-        user.setPassword(MD5Util.getMD5String("123"));
-        user.setType(0);
-        user.setUserName("prf");
-        user.setRegisterDate(new Date(System.currentTimeMillis()));
-        boolean flag = userDao.addUser(user);
-
-        System.out.print(flag);
-        return "/index";
+        return "../homepage";
     }
 }
---
+++ web/WEB-INF/page/null.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: tia
+  Date: 16/5/28
+  Time: 下午12:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
