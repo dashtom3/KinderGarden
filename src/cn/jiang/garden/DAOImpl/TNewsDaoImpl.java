bb60a6de6ba5930811d4286096614172f0168bc0
wawa <46937922@qq.com>
2016-07-10 00:22:04 +0800

    news

--- src/cn/jiang/garden/DAOImpl/TNewsDaoImpl.java
+++ src/cn/jiang/garden/DAOImpl/TNewsDaoImpl.java
@@ -1,6 +1,7 @@
 package cn.jiang.garden.DAOImpl;
 
 import cn.jiang.garden.DAO.BaseDao;
+import cn.jiang.garden.DAO.TFileDao;
 import cn.jiang.garden.DAO.TNewsDao;
 import cn.jiang.garden.model.TFileEntity;
 import cn.jiang.garden.model.TNewsEntity;
@@ -10,6 +11,7 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -20,6 +22,8 @@ import java.util.List;
  */
 @Repository
 public class TNewsDaoImpl extends BaseDao<TNewsEntity> implements TNewsDao {
+    @Autowired
+    TFileDao fileDao;
     @Override
     public boolean addTNews(TNewsEntity tnews){
         return save(tnews);
@@ -52,30 +56,34 @@ public class TNewsDaoImpl extends BaseDao<TNewsEntity> implements TNewsDao {
     @Override
     public DataWrapper<TNewsEntity> getTNews(Long tNewsId){
         DataWrapper<TNewsEntity> retDataWrapper = new DataWrapper<TNewsEntity>();
-        List<Object[]> list = null;
-        TNewsEntity ret = new TNewsEntity();
+        TNewsEntity ret = get(tNewsId);
 
-        String sql = "select {n.*},{c.*} "
-                + "from t_news n,t_file c "
-                + "where n.type = 1 and (n.img_id1 = c.id or n.img_id2 = c.id or n.img_id3 = c.id) and n.id = "+tNewsId+" ";
-        sql += " order by n.type asc";
-        Session session = getSession();
-        Query query = session.createSQLQuery(sql)
-                .addEntity("n",TNewsEntity.class)
-                .addEntity("c",TFileEntity.class);
-        try {
-            list = query.list();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
+
         //添加新闻和横幅图片
-        if(list != null){
-            ret = (TNewsEntity) list.get(0)[0];
-            List<TFileEntity> pic = new ArrayList<TFileEntity>();
-            for(int i =0;i<list.size();i++){
-                pic.add((TFileEntity) list.get(i)[1]);
+        if(ret != null){
+            List<TFileEntity> newsPic = new ArrayList<TFileEntity>();
+            if(ret.getImgId1() != null) {
+                newsPic.add(fileDao.getFileById(ret.getImgId1()));
             }
-            ret.setNewsPic(pic);
+            if(ret.getImgId2() != null) {
+                newsPic.add(fileDao.getFileById(ret.getImgId2()));
+            }
+            if(ret.getImgId3() != null) {
+                newsPic.add(fileDao.getFileById(ret.getImgId3()));
+            }
+            if(ret.getImgId4() != null) {
+                newsPic.add(fileDao.getFileById(ret.getImgId4()));
+            }
+            if(ret.getImgId5() != null) {
+                newsPic.add(fileDao.getFileById(ret.getImgId5()));
+            }
+            if(ret.getImgId6() != null) {
+                newsPic.add(fileDao.getFileById(ret.getImgId6()));
+            }
+            if(ret.getImgId7() != null) {
+                newsPic.add(fileDao.getFileById(ret.getImgId7()));
+            }
+            ret.setNewsPic(newsPic);
         }
         retDataWrapper.setData(ret);
         return retDataWrapper;
